6e48836d464c0d29d366c30f4c50c57d89cb4d90
UbaldoFMelchor <ubaldofmelchor97@hotmail.com>
2022-05-10 01:45:23 -0500

    creada clase PartnerService con modulo para crear una lista de emails de los estudiantes con una certificación

---
+++ test/lib/services/PartnerServices.test.js
@@ -0,0 +1,11 @@
+const PartnerService = require('./../../../lib/services/PartnerServices')
+const Reader = require('./../../../lib/utils/Reader')
+
+describe("Test para PartnerServices", () => {
+    test("Filtrar todos los estudiantes que tenga certificación", () => {
+        const Partners = Reader.readJsonFile("./test/data/VisualPartnersTest.json");
+        const PartnerFilterByCertification = PartnerService.filterByCertification(Partners);
+        expect(PartnerFilterByCertification.length).toBe(4);
+
+    })
+})
