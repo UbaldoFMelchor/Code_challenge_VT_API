d12859cc300f6d4b037a47de6ff9d00dd191424b
UbaldoFMelchor <ubaldofmelchor97@hotmail.com>
2022-05-10 18:33:39 -0500

    cambiado json de test por error en github actions

--- test/lib/services/PartnerServices.test.js
+++ test/lib/services/PartnerServices.test.js
@@ -3,15 +3,15 @@ const Reader = require("./../../../lib/utils/Reader");
 
 describe("Test para PartnerServices", () => {
     test("1) Filtrar todos los estudiantes que tenga certificación", () => {
-        const Partners = Reader.readJsonFile("./VisualPartnersTest.json");
+        const Partners = Reader.readJsonFile("./data/VisualPartners.json");
         const PartnerFilterByCertification = PartnerService.filterByCertification(Partners);
-        expect(PartnerFilterByCertification.length).toBe(4);
+        expect(PartnerFilterByCertification.length).toBe(29);
 
     });
 
     test("2) Filtrar todos los estudiantes que tengan creditos mayores a 500", () => {
-        const Partners = Reader.readJsonFile("./VisualPartnersTest.json");
+        const Partners = Reader.readJsonFile("./data/VisualPartners.json");
         const PartnerWithCreditsUp500 = PartnerService.filterByCredits(Partners);
-        expect(PartnerWithCreditsUp500.length).toBe(3);
+        expect(PartnerWithCreditsUp500.length).toBe(27);
     });
 });
