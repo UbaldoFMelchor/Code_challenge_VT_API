610da63ab57d0b1c39e3d87e0d253f6eb58e86e8
UbaldoFMelchor <ubaldofmelchor97@hotmail.com>
2022-05-10 01:51:11 -0500

    siguen error en github actions, probale solucion?

--- test/lib/services/PartnerServices.test.js
+++ test/lib/services/PartnerServices.test.js
@@ -3,7 +3,7 @@ const Reader = require('./../../../lib/utils/Reader')
 
 describe("Test para PartnerServices", () => {
     test("Filtrar todos los estudiantes que tenga certificación", () => {
-        const Partners = Reader.readJsonFile("VisualPartnersTest.json");
+        const Partners = Reader.readJsonFile("./VisualPartnersTest.json");
         const PartnerFilterByCertification = PartnerService.filterByCertification(Partners);
         expect(PartnerFilterByCertification.length).toBe(4);
 
--- test/lib/utils/reader.test.js
+++ test/lib/utils/reader.test.js
@@ -3,7 +3,7 @@ const Reader = require('./../../../lib/utils/Reader')
 describe("test para carpeta Reader", () => {
     test("1) Probar Reader de datos desde data VisualPartnersTest.json", () =>{
        
-        const reader = Reader.readJsonFile('visualPartnersTest.json')
+        const reader = Reader.readJsonFile('./visualPartnersTest.json')
         expect(reader).toBe(reader)
     })
 })
