ca57d059da7231360995ab602b84f001325007ad
UbaldoFMelchor <ubaldofmelchor97@hotmail.com>
2022-05-10 00:23:42 -0500

    creada Class Reader con su respectivo test

---
+++ test/lib/utils/reader.test.js
@@ -0,0 +1,8 @@
+const Reader = require("./../../../lib/utils/Reader")
+
+describe("test para carpeta Reader", () => {
+    test("1) Probar Reader de datos desde data VisualPartnersTest.json", () =>{
+        const students = Reader.readJsonFile("./test/data/VisualPartnersTest.json")
+        expect(students).toBe(students)
+    })
+})
