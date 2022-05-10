02811a6c2456ce21909de3bf3c0173b5cdb2ada5
UbaldoFMelchor <ubaldofmelchor97@hotmail.com>
2022-05-10 02:03:37 -0500

    creado modulo para filtrar partners con creditos mayores a 500

--- test/lib/services/PartnerServices.test.js
+++ test/lib/services/PartnerServices.test.js
@@ -2,10 +2,16 @@ const PartnerService = require('./../../../lib/services/PartnerServices')
 const Reader = require('./../../../lib/utils/Reader')
 
 describe("Test para PartnerServices", () => {
-    test("Filtrar todos los estudiantes que tenga certificación", () => {
+    test("1) Filtrar todos los estudiantes que tenga certificación", () => {
         const Partners = Reader.readJsonFile("./VisualPartnersTest.json");
         const PartnerFilterByCertification = PartnerService.filterByCertification(Partners);
         expect(PartnerFilterByCertification.length).toBe(4);
 
+    })
+
+    test("2) Filtrar todos los estudiantes que tengan creditos mayores a 500", () => {
+        const Partners = Reader.readJsonFile("./VisualPartnersTest.json");
+        const PartnerWithCreditsUp500 = PartnerService.filterByCredits(Partners);
+        expect(PartnerWithCreditsUp500.length).toBe(3);
     })
 })
