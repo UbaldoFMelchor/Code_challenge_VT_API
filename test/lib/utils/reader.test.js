f34c73661735b1162c3927b2131420d90b23fd64
UbaldoFMelchor <ubaldofmelchor97@hotmail.com>
2022-05-10 00:49:49 -0500

    arreglando github actions

--- test/lib/utils/reader.test.js
+++ test/lib/utils/reader.test.js
@@ -2,7 +2,7 @@ const Reader = require('./../../../lib/utils/Reader')
 
 describe("test para carpeta Reader", () => {
     test("1) Probar Reader de datos desde data VisualPartnersTest.json", () =>{
-        const students = Reader.readJsonFile('./test/data/VisualPartnersTest.json')
+        const students = Reader.readJsonFile("./test/data/VisualPartnersTest.json")
         expect(students).toBe(students)
     })
 })
